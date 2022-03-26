201b26202ffae86e2d161fcb1d7b77ec5f178931
Malek Fahad Amiree <98859571+fahdamiree@users.noreply.github.com>
2022-03-26 12:33:52 -0800

    Update JamesBond.java

--- src/day0/JamesBond.java
+++ src/day0/JamesBond.java
@@ -3,7 +3,7 @@ package day0;
 public class JamesBond{
   
   public static void main(String [] args){
-  System.out.println("James Bond has hacked our project")
+  System.out.println("James Bond has hacked our project");
 
     }
 }
