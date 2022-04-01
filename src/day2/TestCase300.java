cdace8d2154e2f7fc64e87f506a25c7d0d9aa83e
Malek Fahad Amiree <98859571+fahdamiree@users.noreply.github.com>
2022-04-01 12:58:17 -0800

    created the  test case 300

---
+++ src/day2/TestCase300.java
@@ -0,0 +1,11 @@
+package day2;
+
+public class TestCase300 {
+    public static void main(String[] args) {
+
+
+        System.out.println("lovey amriee");
+        System.out.println("my dear love you forever");
+
+    }
+}
