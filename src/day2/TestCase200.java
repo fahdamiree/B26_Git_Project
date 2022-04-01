be9dfd9858f42a65d0458ac68b486d91a1a8d206
Malek Fahad Amiree <98859571+fahdamiree@users.noreply.github.com>
2022-04-01 12:52:02 -0800

    created test case 200 by amiree

---
+++ src/day2/TestCase200.java
@@ -0,0 +1,11 @@
+package day2;
+
+public class TestCase200 {
+    public static void main(String[] args) {
+
+
+        System.out.println("Mohammad");
+        System.out.println("rahim");
+
+    }
+}
