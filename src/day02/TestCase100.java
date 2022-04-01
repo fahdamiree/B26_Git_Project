416113729442cbd339f01be5b715dd5790faa736
Malek Fahad Amiree <98859571+fahdamiree@users.noreply.github.com>
2022-04-01 12:45:46 -0800

    created the test case 100

---
+++ src/day02/TestCase100.java
@@ -0,0 +1,15 @@
+package day02;
+
+public class TestCase100 {
+    public static void main(String[] args) {
+
+
+        System.out.println("Amiree");
+
+        System.out.println("rahimi");
+
+        System.out.println("amani");
+
+
+    }
+}
